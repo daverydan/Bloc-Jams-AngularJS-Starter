d5f37e2b4d4509a270cd6853d590eb76923d59aa
Danny Avery <danny.w.avery@gmail.com>
2017-12-08 12:38:48 -0700

    Add templates + setup ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,24 @@
-angular.module('blocJams', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+    // removes (#!) hashbang mode - prettify routes/url
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false // avoid $location error
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      });
+  }
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
