9bb760dcfec7666e9ee01fe7face51ca19b9beb5
Danny Avery <danny.w.avery@gmail.com>
2017-12-09 09:21:34 -0700

    Add SongPlayer service + debug issue

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,37 @@
+(function() {
+  function SongPlayer() {
+    var SongPlayer = {};
+
+    var currentSong = null;
+    var currentBuzzObject = null;
+
+    SongPlayer.play = function(song) {
+      if (currentSong !== song) {
+        if (currentBuzzObject) {
+          currentBuzzObject.stop();
+          currentSong.playing = null;
+        } else if (currentSong === song) {
+          if (currentBuzzObject.isPaused()) {
+            currentBuzzObject.play();
+            car.playing = true;
+          }
+        }
+
+        currentBuzzObject = new buzz.sound(song.audioUrl, {
+          formats: ['mp3'],
+          preload: true
+        });
+
+        currentSong = song;
+
+        currentBuzzObject.play();
+      }
+    };
+
+    return SongPlayer;
+  }
+
+  angular
+    .module('blocJams')
+    .factory('SongPlayer', SongPlayer);
+})();
